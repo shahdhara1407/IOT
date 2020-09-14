da08b38fc71141638294563c8576936f5c072f27
shahdhara1407 <71249716+shahdhara1407@users.noreply.github.com>
2020-09-14 13:11:51 +0530

    Create PRAC1.py

---
+++ PRAC1.py
@@ -0,0 +1,14 @@
+#IOT
+token = '1224691054:AAEwlieSKODF3wEAISagprMWeABei04gT5w'
+TelegramBot = telepot.Bot(token)
+print (TelegramBot.getMe())
+def handle(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    print(content_type, chat_type, chat_id)
+
+    if content_type == 'text':
+        bot.sendMessage(chat_id, "You said '{}'".format(msg["text"]))
+
+
+TelegramBot.message_loop(handle)
+
